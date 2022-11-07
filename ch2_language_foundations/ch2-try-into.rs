321cc0620c94b92239c3b7870f47a2f2763eb6bd
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-11-08 06:39:22 +0700

    feat: initial commit

---
+++ ch2_language_foundations/ch2-try-into.rs
@@ -0,0 +1,13 @@
+use std::convert::TryInto;
+
+fn main() {
+  let a: i32 = 10;
+  let b: u16 = 100;
+
+  let b_ = b.try_into()
+            .unwrap();
+
+  if a < b_ {
+    println!("Ten is less than one hundred.");
+  }
+}
