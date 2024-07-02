12b9d38b193207f97b8497d47054994cdabb7885
Sutthinart Khunvadhana <iakgoog@gmail.com>
2024-07-02 20:32:07 +0700

    feat: Improve 3.3.1

--- ch3_compound_data_types/src/main.rs
+++ ch3_compound_data_types/src/main.rs
@@ -17,6 +17,20 @@ mod ch3 {
                 data: Vec::new(),
             }
         }
+
+        fn new_with_data(name: &str, data: &Vec<u8>) -> Filez {
+            let mut f= Filez::new(name);
+            f.data = data.clone();
+            f
+        }
+
+        fn read(self: &Filez, save_to: &mut Vec<u8>) -> usize {
+            let mut tmp = self.data.clone();
+            let read_length = tmp.len();
+            save_to.reserve(read_length);
+            save_to.append(&mut tmp);
+            read_length
+        }
     }
 
     // type File = String;
@@ -110,13 +124,22 @@ mod ch3 {
     }
 
     pub fn p331() {
-        let f3 = Filez::new("f3.txt");
+        let f3_data: Vec<u8> = vec![
+            114, 117, 115, 116, 33, 64
+        ];
+        let mut f3 = Filez::new_with_data("3.txt", &f3_data);
 
-        let f3_name = &f3.name;
-        let f3_length = f3.data.len();
+        let mut buffer: Vec<u8> = vec![];
+
+        open(&mut f3);
+        let f3_length = f3.read(&mut buffer);
+        close(&mut f3);
+
+        let text = String::from_utf8_lossy(&buffer);
 
         println!("{:?}", f3);
-        println!("{} is {} bytes long", f3_name, f3_length);
+        println!("{} is {} bytes long", &f3.name, f3_length);
+        println!("{}", text);
     } 
 }
 
